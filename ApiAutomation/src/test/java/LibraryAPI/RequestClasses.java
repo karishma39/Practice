2033b3850270db6252d86675db527b9357444445
karishma39 <karishmaoberoi94@gmail.com>
2021-05-23 20:42:56 +0530

    API automation prac

---
+++ ApiAutomation/src/test/java/LibraryAPI/RequestClasses.java
@@ -0,0 +1,77 @@
+package LibraryAPI;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import API.automation.ApiAutomation.Books;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class RequestClasses {
+	
+	Books book=new Books();
+	
+	@Test(priority = 1)
+	public void getBooks()
+	{
+		RestAssured.baseURI="https://rahulshettyacademy.com";
+		
+		RequestSpecification spec=RestAssured.given();
+		
+		spec.queryParam("ID", "3389");
+		Response response = spec.get("/Library/GetBook.php");
+		
+		System.out.println(response.asPrettyString());
+		
+	}
+	
+	@Test(priority = 2)
+	public void addBooks()
+	{
+		
+		book.setAisle("226");
+		book.setAuthor("John foe");
+		book.setName("Learn Appium Automation with Java");
+		book.setIsbn("bcdd");
+		
+		RequestSpecification spec=RestAssured.given();
+		spec.body(book);
+		
+		Response response = spec.post("/Library/Addbook.php");
+		
+		System.out.println(response.asPrettyString());
+		Assert.assertEquals(response.getStatusCode(), 200);
+		
+		JsonPath json = response.jsonPath();
+		Assert.assertEquals(json.getString("Msg"), "successfully added");
+		
+	}
+	
+	@Test(priority = 3)
+	public void delBook()
+	{
+		RequestSpecification spec=RestAssured.given();
+		spec.body("{\r\n"
+				+ "    \"ID\": \"bcdd226\"\r\n"
+				+ "}");
+		
+		Response response = spec.post("/Library/DeleteBook.php");
+		
+		System.out.println(response.asPrettyString());
+		Assert.assertEquals(response.getStatusCode(), 200);
+		
+		JsonPath json = response.jsonPath();
+		Assert.assertEquals(json.getString("msg"), "book is successfully deleted");
+	}
+	
+	
+
+}
