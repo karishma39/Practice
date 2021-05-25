1d97e18e8b88c839fe8026efec3a867461d0a724
karishma39 <karishmaoberoi94@gmail.com>
2021-05-25 16:18:04 +0530

    Java Ques

---
+++ JavaPractice/src/Prac1/ArraySqaureAndSort.java
@@ -0,0 +1,27 @@
+package Prac1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ArraySqaureAndSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//		Given an array of integers sorted in ascending order, return an array of the squares of each number sorted in ascending order.
+		//		**Input**: [-2, -1, 0, 1, 2, 3]
+		//		**Output**: [0, 1, 1, 4, 4, 9]
+
+		int arr[]=new int[] {-2, -1, 0, 1, 2, 3};
+		List list=new ArrayList();
+		for(int i=0;i<arr.length;i++)
+		{
+			list.add(arr[i]*arr[i]);
+		}
+		Collections.sort(list);
+		System.out.println(list);
+
+	}
+
+}
---
+++ JavaPractice/src/Prac1/Sum2NosWithoutAnyOperator.java
@@ -0,0 +1,31 @@
+package Prac1;
+
+public class Sum2NosWithoutAnyOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=5;
+		int b=4;
+		
+		for(int i=0;i<b;i++)
+		{
+			++a;
+		}
+		
+		System.out.println(a);
+		
+		//if 2nd no is negative
+		int d=-9;
+		int c=-4;
+		while(c<0)
+		{
+			d--;
+			c++;
+		}
+		
+		System.out.println(d);
+
+	}
+
+}
