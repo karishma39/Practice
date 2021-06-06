fff02ec63da9c6413dcb657ca9c65f6a64161c4c
karishma39 <karishmaoberoi94@gmail.com>
2021-06-07 01:44:24 +0530

    CODE

---
+++ JavaPractice/src/Prac1/ArrayKingElement.java
@@ -0,0 +1,36 @@
+package Prac1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayKingElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[] = new int[] {5,5,5,3};
+		//o/p: 20, 18, 15, 6
+		
+		List list= new ArrayList();
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			int j;
+			for(j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]<=arr[j])
+				{
+					break;
+				}
+			}
+			if(j==arr.length)
+			{
+				list.add(arr[i]);
+			}
+		}
+		
+		System.out.println(list);
+
+	}
+
+}
